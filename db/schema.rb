e530b80ce33f7b2ab10b25915766d3ffc9de2f48
Hiro Mataba <aganzemataba@gmail.com>
2021-10-30 13:34:51 +0200

    Rake database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_30_105742) do
+ActiveRecord::Schema.define(version: 2021_10_30_112721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,8 @@ ActiveRecord::Schema.define(version: 2021_10_30_105742) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
@@ -49,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_10_30_105742) do
     t.text "bio"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "posts_counter", default: 0, null: false
   end
 
   add_foreign_key "comments", "posts"
