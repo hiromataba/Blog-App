5636d9db5512c671a94a1e9dc56bfd8d7f60efa7
Hiro Mataba <aganzemataba@gmail.com>
2021-11-06 16:25:03 +0200

    Add spec for posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,35 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET #index' do
-    before(:example) { get user_posts_path(1) }
+  let(:user) { User.create(name: 'Barack') }
+  let(:post) { user.posts.create(title: 'Post', comments_counter: 2, likes_counter: 4) }
 
-    it 'should have correct response status' do
+  describe 'GET #index' do
+    before { get user_posts_path(post.user_id) }
+
+    it 'should have response status correct(ok)' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should render correct template' do
-      expect(response).to render_template(:index)
+    it "renders 'index' template" do
+      expect(response).to render_template('index')
     end
 
-    it 'should include correct placeholder text' do
-      expect(response.body).to include('Here is a list of posts for a given post index')
+    it 'should include correct placeholder' do
+      expect(response.body).to include('Pagination')
     end
   end
 
   describe 'GET #show' do
-    before(:example) { get user_post_path(1, 1) }
+    before do
+      get user_post_path(user_id: user.id, id: post.id)
+    end
 
-    it 'should have correct response status' do
+    it 'should have response status correct(ok)' do
       expect(response).to have_http_status(:ok)
     end
 
-    it 'should render correct template' do
-      expect(response).to render_template(:show)
-    end
-
-    it 'should include correct placeholder text' do
-      expect(response.body).to include('Here is a list of posts for a given post show')
+    it "renders 'show' template" do
+      expect(response).to render_template('show')
     end
   end
 end
